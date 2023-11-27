1a7d1508ec5f25ffe5f24c453ca991853af00ba8
nijesmik <bkybb@duck.com>
2023-11-27 20:08:47 +0900

    Feat: Exam Rank 04

---
+++ Exam/Rank04/microshell.c
@@ -0,0 +1,73 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int cd(char **av, int i);
+int exec(char **av, char **envp, int i);
+
+int main(int ac, char ** av, char ** envp) {
+	int status = 0;
+	int i = 0;
+
+	if (ac > 1) {
+		while (av[i] && av[++i]) {
+			av += i;
+			i = 0;
+			while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";")) {
+				i++;
+			}
+			if (!strcmp(*av, "cd")) {
+				status = cd(av, i);
+			} else if (i) {
+				status = exec(av, envp, i);
+			}
+		}
+	}
+	return status;
+}
+
+int err(char *str) {
+	while (*str) {
+		write(2, str++, 1);
+	}
+	return 1;
+}
+
+int cd(char **av, int i) {
+	if (i != 2) {
+		return err("error: cd: bad arguments\n");
+	} else if (chdir(av[1]) == -1) {
+		return err("error: cd: cannot change directory to"), err(av[1]), err("\n");
+	}
+	return 0;
+}
+
+int exec(char **av, char **envp, int i) {
+	int fd[2];
+	int status;
+	int hasPipe = av[i] && !strcmp(av[i], "|");
+
+	// open pipe
+	if (hasPipe && pipe(fd) == -1) {
+		return err("error: fatal\n");
+	}
+
+	int pid = fork();
+
+	// child process
+	if (!pid) {
+		av[i] = 0;
+		if (hasPipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1)) {
+			return err("error: fatal\n");
+		}
+		execve(*av, av, envp);
+		return err("error: cannot execute "), err(*av), err("\n");
+	}
+
+	// parent process
+	if (hasPipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1)) {
+		return err("error: fatal\n");
+	}
+	waitpid(pid, &status, 0);
+	return WIFEXITED(status) && WEXITSTATUS(status);
+}
